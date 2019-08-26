bec16378e142d35ad8f12515e355a7a867b2189a
Masster <andrei80586@gmail.com>
2019-08-26 20:08:41 +0300

    addition

--- app/src/main/java/com/masstersoft/serviceapp/FirstService.kt
+++ app/src/main/java/com/masstersoft/serviceapp/FirstService.kt
@@ -7,21 +7,23 @@ import android.util.Log
 
 class FirstService : Service() {
     val LOG_TAG = "ServiceAPP"
+    var counter: Int = 0
 
     override fun onCreate() {
         super.onCreate()
         Log.d(LOG_TAG, "onCreate FirstService")
+        counter++
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d(LOG_TAG, "onStartCommand FirstService counter = $counter")
+        someWork()
+        return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         Log.d(LOG_TAG, "onDestroy FirstService")
-    }
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d(LOG_TAG, "onStartCommand FirstService")
-        someWork()
-        return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onBind(intent: Intent): IBinder? {
