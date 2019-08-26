0edc2a7cb5dcb1965111d1ed803475bf766ee485
Masster <andrei80586@gmail.com>
2019-08-26 20:03:17 +0300

    StartService and StopService

--- app/src/main/java/com/masstersoft/serviceapp/MainActivity.kt
+++ app/src/main/java/com/masstersoft/serviceapp/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.masstersoft.serviceapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
     }
+
+    private fun setListeners() {
+        btnStart.setOnClickListener {
+            startService(Intent(this, FirstService::class.java))
+        }
+        btnStop.setOnClickListener {
+            stopService(Intent(this, FirstService::class.java))
+        }
+    }
+
 }
